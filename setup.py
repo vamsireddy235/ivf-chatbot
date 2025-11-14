646b8c3f0def041a2ddb1994e864a126b9d6fc6c
Vamsi <vamsiextension235@gmail.com>
2025-11-14 23:25:13 +0530

    requirement added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="IVF-CHATBOT",
+    version="0.1.0",
+    author="VAMSI REDDY",
+    author_email="VAMSIEXTENSION235@GMAIL.COM",
+    packages=find_packages(),
+    install_requires=[]
+)
